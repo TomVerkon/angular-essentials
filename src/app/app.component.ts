3b401a864a63fcf3d3d5eb020a50693b77e4bc91
TomVerkon <tverkon@gmail.com>
2024-11-01 17:04:47 -0400

    finished tasks component exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-user';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  name = 'non-selected';
+
+  onSelectUser(name: string) {
+    this.name = name;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,24 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  // traditional parameter passing
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+  // new signal parameter passing
+  // id = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
+    // return 'assets/users/' + this.avatar();
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.name);
+    // this.select.emit(this.id());
+  }
 }
